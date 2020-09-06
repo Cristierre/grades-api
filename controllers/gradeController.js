451ea5660afc1f08bdefdf08245d4ec7482d25e7
Critierre Gomes Konrath <cristierrekonrath@gmail.com>
2020-09-05 23:26:13 -0300

    Comitando projeto inicial

---
+++ controllers/gradeController.js
@@ -0,0 +1,100 @@
+import { db } from "../models/index.js";
+import { logger } from "../config/logger.js";
+import { gradeModel } from "../models/gradeModel";
+
+const gradeModel = db.grade;
+
+const create = async (req, res) => {
+  const { name, subject, type, value } = req.body;
+
+  const grade = new gradeModel({
+    name: name,
+    subject: subject,
+    type: type,
+    value: value,
+    lastModified: new Date.now(),
+  });
+  try {
+    const data = await grade.save();
+    res.status(201).send();
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Algum erro ocorreu ao salvar" });
+    logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findAll = async (req, res) => {
+  const name = req.query.name;
+
+  //condicao para o filtro no findAll
+  var condition = name
+    ? { name: { $regex: new RegExp(name), $options: "i" } }
+    : {};
+  try {
+    const grades = await gradeModel.find(condition);
+    res.send(grades);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || "Erro ao listar todos os documentos" });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const grade = await gradeModel.findById({ _id: id });
+
+    res.send(grade);
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao buscar o Grade id: " + id });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const update = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Dados para atualizacao vazio",
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+   const grade = await gradeModel.findByIdAndUpdate({ _id: id }, req.body, {new: true});
+
+   res.sendStatus(201).send(grade);
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao atualizar a Grade id: " + id });
+    logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await gradeModel.findByIdAndRemove({ _id: id });
+    res.sendStatus(404);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Nao foi possivel deletar o Grade id: " + id });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const removeAll = async (req, res) => {
+  try {
+    await gradeModel.deleteMany();
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao excluir todos as Grades" });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+export default { create, findAll, findOne, update, remove, removeAll };
